681431f8d44f555a0603a01966b85380a10263fb
vmoeak <dangpengfei123@gmail.com>
2021-04-14 18:42:07 +0800

    topnav样式

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./index.scss";
 import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "./view/Home.vue";
 import Doc from "./view/Doc.vue";

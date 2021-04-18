cfe72332b7e5aeaacd8c3d7369481239c9a43eb0
vmoeak <dangpengfei123@gmail.com>
2021-04-19 03:11:26 +0800

    dialog组件样式

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,14 +1,24 @@
 <template>
-  <Dialog />
+  <Dialog v-model:visible="y" />
+  <Button @click="toggle">toggle</Button>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
 export default defineComponent({
   name: "DialogDemo",
   components: {
     Dialog,
+    Button,
+  },
+  setup() {
+    const y = ref(false);
+    const toggle = () => {
+      y.value = !y.value;
+    };
+    return { y, toggle };
   },
 });
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button>OK</Button>
-      <Button>Cancel</Button>
-    </footer>
+  <div v-if="visible">
+    <div class="ui-dialog-overlay"></div>
+    <div class="ui-dialog-wrapper">
+      <div class="ui-dialog">
+        <header>标题 <span class="ui-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,8 +25,78 @@ export default defineComponent({
   components: {
     Button,
   },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
 });
 </script>
 
 <style lang="scss" scoped>
+$radius: 4px;
+$border-color: #d9d9d9;
+.ui-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+  > main {
+    padding: 12px 16px;
+  }
+  > footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
 </style>

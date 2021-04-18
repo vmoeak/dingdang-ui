d13404c823c9a8a1e2892591369007af2abffa27
vmoeak <dangpengfei123@gmail.com>
2021-04-18 22:04:39 +0800

    switch组件添加样式

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>Switch 的文档</div>
+  <Switch />
 </template>
+
+<script lang='ts'>
+import Switch from "../lib/Switch.vue";
+export default {
+  name: "SwitchDemo",
+  components: {
+    Switch,
+  },
+};
+</script>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,49 @@
+<template>
+  <button @click="onClick"><span :class="{ transition: x }"></span></button>
+</template>
+
+<script lang='ts'>
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "Switch",
+  setup() {
+    const x = ref(false);
+    const onClick = () => {
+      x.value = !x.value;
+    };
+    return {
+      x,
+      onClick,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: blue;
+  border-radius: $h/2;
+  position: relative;
+}
+span {
+  // position: absolute;
+  // top: 2px;
+  // left: 2px;
+  display: block;
+  height: $h2;
+  width: $h2;
+  background: white;
+  margin-left: 2px;
+  margin-right: 2px;
+  border-radius: $h2 / 2;
+  transition: margin-left 1s;
+}
+.transition {
+  margin-left: auto;
+}
+</style>

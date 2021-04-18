53db073005179655e1ec2705a6337f59eca2e78d
vmoeak <dangpengfei123@gmail.com>
2021-04-19 01:45:06 +0800

    将组件库样式加上前缀

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,7 @@
 <template>
-  <button @click="toggle" :class="{ checked: value }"><span></span></button>
+  <button class="ui-switch" @click="toggle" :class="{ 'ui-checked': value }">
+    <span></span>
+  </button>
 </template>
 
 <script lang='ts'>
@@ -23,36 +25,36 @@ export default defineComponent({
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button {
+.ui-switch {
   height: $h;
   width: $h * 2;
   border: none;
   background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  display: block;
-  height: $h2;
-  width: $h2;
-  background: white;
-  margin-left: 2px;
-  margin-right: 2px;
-  border-radius: $h2 / 2;
-  transition: all 250ms;
-}
-button.checked {
-  background: #1890ff;
-}
-button.checked > span {
-  margin-left: auto;
-}
-button:focus {
-  outline: none;
-}
-button:active {
   > span {
-    width: $h2 + 4px;
+    display: block;
+    height: $h2;
+    width: $h2;
+    background: white;
+    margin-left: 2px;
+    margin-right: 2px;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+  &.ui-checked {
+    background: #1890ff;
+  }
+  &.ui-checked > span {
+    margin-left: auto;
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
   }
 }
 </style>

6e8fbd5366ae15cef28166b16a9120e7f39162a1
vmoeak <dangpengfei123@gmail.com>
2021-04-19 02:20:54 +0800

    button组件支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -15,13 +15,18 @@ export default defineComponent({
       type: String,
       default: "normal",
     },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
   setup(props, ctx) {
-    const { theme, size } = props;
+    const { theme, size, level } = props;
     const classes = computed(() => {
       return {
         [`ui-theme-${theme}`]: theme,
         [`ui-size-${size}`]: size,
+        [`ui-level-${level}`]: level,
       };
     });
     return {
@@ -36,6 +41,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .ui-button {
   box-sizing: border-box;
   height: $h;
@@ -50,6 +56,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -82,16 +89,61 @@ $radius: 4px;
       background: darken(white, 5%);
     }
   }
+  &.ui-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.ui-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
   &.ui-theme-button {
-    &.ui-size-big {
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px;
+    &.ui-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.ui-size-small {
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+    &.ui-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.ui-theme-link {
+    &.ui-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.ui-theme-text {
+    &.ui-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.ui-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
 }

900cb2a93bd45ecce85229136c8bc94994a95fce
vmoeak <dangpengfei123@gmail.com>
2021-04-20 01:53:46 +0800

    添加文档说明

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,60 +1,23 @@
 <template>
-  <Button @click="toggle">toggle</Button>
-  <Dialog
-    v-model:visible="y"
-    :closeOnClickOverlay="false"
-    :ok="f1"
-    :cancel="f2"
-  >
-    <template v-slot:title>
-      <strong>加粗的标题</strong>
-    </template>
-    <template v-slot:content>
-      <div>hi</div>
-    </template>
-  </Dialog>
-  <h1>示例2</h1>
-  <Button @click="showDialog">show</Button>
+  <div>Dialog 示例</div>
+  <Demo :component="DialogDemo1" />
+  <Demo :component="DialogDemo2" />
 </template>
 
 <script lang='ts'>
 import { defineComponent, ref, h } from "vue";
-import Dialog from "../lib/Dialog.vue";
-import Button from "../lib/Button.vue";
-import { openDialog } from "../lib/openDialog";
+import Demo from "./Demo.vue";
+import DialogDemo1 from "./DialogDemo1.vue";
+import DialogDemo2 from "./DialogDemo2.vue";
 export default defineComponent({
   name: "DialogDemo",
   components: {
-    Dialog,
-    Button,
+    Demo,
   },
   setup() {
-    const y = ref(false);
-    const toggle = () => {
-      y.value = !y.value;
-    };
-    const f1 = () => {
-      return false;
-    };
-    const f2 = () => {};
-    const showDialog = () => {
-      openDialog({
-        title: h("strong", {}, "标题"),
-        content: "你好",
-        ok() {
-          console.log("ok");
-        },
-        cancel() {
-          console.log("cancel");
-        },
-      });
-    };
     return {
-      y,
-      toggle,
-      f1,
-      f2,
-      showDialog,
+      DialogDemo1,
+      DialogDemo2,
     };
   },
 });

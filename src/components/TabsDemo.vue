d52713697d0d9a68b1abca894bcabad3b7178251
vmoeak <dangpengfei123@gmail.com>
2021-04-20 07:38:44 +0800

    修改TAbsDemo

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,26 +1,20 @@
 <template>
-  <div>
-    <Tabs v-model:selected="y">
-      <Tab title="first">内容1</Tab>
-      <Tab title="second">内容2</Tab>
-    </Tabs>
-  </div>
+  <h1>Tabs 示例</h1>
+  <Demo :component="TabsDemo1" />
 </template>
 
 <script lang='ts'>
 import { defineComponent, ref } from "vue";
-import Tab from "../lib/Tab.vue";
-import Tabs from "../lib/Tabs.vue";
+import Demo from "./Demo.vue";
+import TabsDemo1 from "./TabsDemo1.vue";
 export default defineComponent({
   name: "TabsDemo",
   components: {
-    Tab,
-    Tabs,
+    Demo,
   },
   setup() {
-    const y = ref("first");
     return {
-      y,
+      TabsDemo1,
     };
   },
 });

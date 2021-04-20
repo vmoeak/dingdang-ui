27b42ad8cdab01b2ede0e6467ca56ced94981685
vcurry123 <dangpengfei123@icloud.com>
2021-04-20 22:40:53 +0800

    app组件使用script setup

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,17 @@
   <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, provide } from "vue";
+<script lang="ts" setup>
+import { ref, provide } from "vue";
 import { router } from "./router";
 
-export default defineComponent({
-  name: "App",
-  setup() {
-    const width = document.documentElement.clientWidth;
-    const menuVisible = ref(width <= 500 ? false : true);
-    provide("menuVisible", menuVisible);
-    router.afterEach(() => {
-      if (width <= 500) {
-        menuVisible.value = false;
-      }
-    });
-  },
+const width = document.documentElement.clientWidth;
+const menuVisible = ref(width <= 500 ? false : true);
+provide("menuVisible", menuVisible);
+router.afterEach(() => {
+  if (width <= 500) {
+    menuVisible.value = false;
+  }
 });
 </script>
 

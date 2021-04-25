2d0dd2f295bfa26979135bde81e419788c862090
vmoeak <dangpengfei123@gmail.com>
2021-04-25 20:50:15 +0800

    删除多余代码

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github2:vcurry123/ui-website.git &&
+git remote add origin git@github:vmoeak/dingdang-ui-website.git &&
 git push -f -u origin master &&
 cd -
